bba28f0551b95b6341c2477095e92c2a8add8b33
Aaron Chan <develop@aeccue.com>
2023-08-15 16:41:23 +0900

    Addon that adds audio reading to Japanese notes
    This addon will first look for the audio reading of the selected
    Japanese note using the kanji and kana. If the audio is found, it will
    be downloaded to the media directory with the note's id as the filename.
    The filename is then added to the audio field of the note.

---
+++ japanese-audio-clips/__init__.py
@@ -0,0 +1,82 @@
+import requests
+from anki.hooks import addHook
+from aqt import mw
+from aqt.qt import *
+
+config = mw.addonManager.getConfig(__name__)
+
+KANA_FIELD = config["kanaField"]
+KANJI_FIELD = config["kanjiField"]
+AUDIO_FIELD = config["audioField"]
+
+AUDIO_SOURCE = "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kana={kana}&kanji={kanji}"
+
+
+# ************************************************
+#              Audio lookup function             *
+# ************************************************
+
+# gets audio file from online source
+def find_audio(kana, kanji):
+    resp = requests.get(AUDIO_SOURCE.format(kana=kana, kanji=kanji))
+    # ignore audio that says that no audio is currently available (check based on file size)
+    if len(resp.content) == 52288:
+        raise NoAudioClipFoundException()
+
+    return resp.content
+
+
+class NoAudioClipFoundException(Exception):
+    pass
+
+
+# ************************************************
+#                  Interface                     *
+# ************************************************
+
+# add menu entry to browser window
+def setup_browser_menu(browser):
+    a = QAction("Add Audio", browser)
+    a.triggered.connect(lambda: add_audio(browser.selectedNotes()))
+    browser.form.menuEdit.addSeparator()
+    browser.form.menuEdit.addAction(a)
+
+
+# ************************************************
+#              Hooked functions                  *
+# ************************************************
+
+# adds audio to selected notes
+def add_audio(nids):
+    mw.checkpoint("Add Audio")
+    mw.progress.start()
+
+    for nid in nids:
+        note = mw.col.get_note(nid)
+
+        try:
+            # finds audio, and if found, writes it to file in media directory
+            audio = find_audio(note[KANA_FIELD], note[KANJI_FIELD])
+            mw.col.media.write_data(str(nid) + ".mp3", audio)
+        except NoAudioClipFoundException:
+            # if no audio is found, add no-audio tag
+            note.add_tag("no-audio")
+            note.flush()
+            continue
+
+        try:
+            # writes audio file name to Anki note field
+            note[AUDIO_FIELD] = "[sound:" + nid + ".mp3]"
+        except KeyError:
+            pass
+
+        note.flush()
+
+    mw.progress.finish()
+    mw.reset()
+
+
+# ************************************************
+#                    Main                        *
+# ************************************************
+addHook("browser.setupMenus", setup_browser_menu)
---
+++ japanese-audio-clips/config.md
@@ -0,0 +1,5 @@
+*kanjiField*: Field of the word to look up audio for (case-sensitive).
+
+*kanaField*: Field of the reading of the word to look up audio for (case-sensitive).
+
+*audioField*: Field to put the audio.
